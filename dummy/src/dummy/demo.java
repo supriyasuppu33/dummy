86809d71b5cc41a76d44d22ffb0c64be26bd0be2
delegate <anushathummapudi95@gmail.com>
2018-07-25 16:32:02 +0530

    potato commit

--- dummy/src/dummy/demo.java
+++ dummy/src/dummy/demo.java
@@ -6,6 +6,7 @@ public class demo {
 		System.out.println("hhhhhhhhhhhhaaaaaaaaaiiiiii");
 		System.out.println("hello brinjal");
 		System.out.println("haaaai");
+		System.out.println("potato");
 	}
 
 }
